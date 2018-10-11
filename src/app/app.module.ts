f9873cb18a638f2b8fe9cc4f22d0556ed209d9f5
Jean-Carlo <jeancarlo@fitco.com.pe>
2018-10-11 15:01:01 -0500

    Kari-y Giane.. frends pepas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,19 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+
+var config = {
+  apiKey: "AIzaSyDVaB-O6zFd04JXVt-iQkog7N9Hb1gt4nI",
+  authDomain: "chatw-bf42d.firebaseapp.com",
+  databaseURL: "https://chatw-bf42d.firebaseio.com",
+  projectId: "chatw-bf42d",
+  storageBucket: "",
+  messagingSenderId: "953190867830"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +24,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireDatabaseModule,
+    AngularFireModule,
+    AngularFireModule.initializeApp(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {AngularFireDatabase} from 'angularfire2/database'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+    messages : any[];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public firebase : AngularFireDatabase) {    
+    this.getDataFirebase();
+  }
 
+  getDataFirebase(){
+    this.firebase.list('/HEROES/').valueChanges().subscribe(
+      data=>{
+        this.messages = data
+      }
+    )
   }
 
 }
